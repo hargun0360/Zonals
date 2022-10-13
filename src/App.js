0aa5243a5d8e782b8a3fd7918048a733fcc76c74
hargun0360 <hargunsingh0720@gmail.com>
2022-10-14 04:37:58 +0530

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
-
+import Navbar from './Navbar/Navbar';
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <Routes>
+      <Route exact path="/" element={<Navbar />}/>
+      {/* <Route exact path="/about" element={<About/>}/>
+      <Route exact path="/registration" element={<Registration/>}/>
+      <Route exact path="/important-dates" element={<ImpDates/>}/>
+      <Route exact path="/programme-committee" element={<Committee/>}/> */}
+    </Routes>
     </div>
   );
 }
