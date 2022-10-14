f113730448ba86a533da60f60e7c4389ce89af7a
hargun0360 <hargunsingh0720@gmail.com>
2022-10-14 22:42:43 +0530

    Add Footer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork';
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import WebIcon from '@mui/icons-material/Web';
+
+const Footer = () => {
+    return (<>
+
+        <div className='Footer'>
+            <div className='contact-us'>
+                <div className='contact-us-details'>
+                    <div className='contact-heading'>
+                        <h1>Contact Us</h1>
+                    </div>
+                    <div className='college-name'>
+                        <div className='icon'>
+                            <MapsHomeWorkIcon fontSize='large'></MapsHomeWorkIcon>
+                        </div>  
+                        <div className='text'>
+                            <p>AJAY KUMAR GARG ENGINEERING COLLEGE</p>
+                        </div>
+                    </div>
+                    <div className='college-address'>
+                        <div className='icon'>
+                            <FmdGoodIcon fontSize='large'></FmdGoodIcon>
+                        </div>
+                        <div className='text'>
+                            <p>27<sup>th</sup>KM STONE, NH-24 DELHI-HAPUR BYPASS ROAD</p>
+                            <p>ADHYATMIK NAGAR GHAZIABAD - 201009</p>
+                        </div>
+                    </div>
+                    <div className='mobile-number'>
+                        <div className='icon'>
+                            <LocalPhoneIcon fontSize='large'></LocalPhoneIcon>
+                        </div>
+                        <div className='text'>
+                            <p>8744052891 TO 94, 7290034976</p>
+                        </div>
+                    </div>
+                    <div className='site-link'>
+                        <div className='icon'>
+                            <WebIcon fontSize='large'></WebIcon>
+                        </div>
+                        <div className='text'>
+                            <p>
+                                <span style={{cursor:"pointer"}} >www.akgec.in/tabletennistournament</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='QR-50'>
+                <div className='QR-Box'>
+                    
+                </div>
+            </div>
+        </div>
+
+    </>
+    )
+}
+
+export default Footer
